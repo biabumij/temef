f9257507a0aa10400e9c74f1748e98feee9b6baf
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-07-06 21:07:58 +0700

    update

--- application/modules/laporan/controllers/Laporan.php
+++ application/modules/laporan/controllers/Laporan.php
@@ -261,7 +261,7 @@ class Laporan extends Secure_Controller {
 			
 			$data['filter_date'] = $filter_date;
 
-		$this->db->select('ppp.client_id, ppp.nama_pelanggan as nama, (ppp.total) as jumlah');
+		$this->db->select('ppp.client_id, ppp.nama_pelanggan as nama, SUM(ppp.total) as jumlah');
 		if(!empty($start_date) && !empty($end_date)){
             $this->db->where('ppp.tanggal_invoice >=',$start_date);
             $this->db->where('ppp.tanggal_invoice <=',$end_date);
