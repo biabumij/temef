5bf63ceb021c890fc20a803ebfa9fa9d1677131a
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-07-28 10:06:17 +0700

    script

--- application/modules/pmm/models/Pmm_model.php
+++ application/modules/pmm/models/Pmm_model.php
@@ -2426,7 +2426,7 @@ class Pmm_model extends CI_Model {
 
                 $receipt = $this->db->select('SUM(volume) as total')->get_where('pmm_receipt_material',array('purchase_order_id'=>$row['id']))->row_array();
                 $total_receipt = $this->pmm_model->GetTotalReceipt($row['id']);
-                $row['receipt'] = '<a href="'.site_url('pmm/purchase_order/receipt_material_pdf/'.$row['id']).'" target="_blank" >'.number_format($receipt['total'],2,',','.').'</a>';
+                $row['receipt'] = number_format($receipt['total'],2,',','.');
                 $row['total_receipt'] = number_format($total_receipt,0,',','.');
                 $row['total_receipt_val'] = $total_receipt;
 
