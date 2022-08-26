91c9cfc82c0284c76cf160299f3a395289e8ed7e
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-08-26 09:43:11 +0700

    sales po

--- application/modules/penjualan/views/cetak_penagihan_penjualan.php
+++ application/modules/penjualan/views/cetak_penagihan_penjualan.php
@@ -142,7 +142,6 @@
 				if($dt['tax_id'] == 6){
                     $tax_ppn11 += $dt['tax'];
                 }
-               // $total += $subtotal;
            	}
            	?>
             <tr>
@@ -260,12 +259,12 @@
 							</td>
 						</tr>
 						<tr>
-						     <?php
-                                $this->db->select('g.admin_group_name');
-                                $this->db->join('tbl_admin_group g','a.admin_group_id = g.admin_group_id','left');
-                                $this->db->where('a.admin_id',$penagihan['created_by']);
-                                $created_group = $this->db->get('tbl_admin a')->row_array();
-                                ?>
+						    <?php
+							$this->db->select('g.admin_group_name');
+							$this->db->join('tbl_admin_group g','a.admin_group_id = g.admin_group_id','left');
+							$this->db->where('a.admin_id',$penagihan['created_by']);
+							$created_group = $this->db->get('tbl_admin a')->row_array();
+							?>
 							<td align="center">
 							    <b><?php echo $this->crud_global->GetField('penerima',array('id'=>$penagihan['client_id']),'nama');?></b>
 							</td>
