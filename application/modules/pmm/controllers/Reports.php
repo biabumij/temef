b4ff926656ef68e23533d9eb536675aca7d40f04
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-09-01 16:46:40 +0700

    buku besar

--- application/modules/pmm/controllers/Reports.php
+++ application/modules/pmm/controllers/Reports.php
@@ -4081,6 +4081,45 @@ class Reports extends CI_Controller {
 			$total_kredit_bensin_tol_parkir_all = $total_kredit_bensin_tol_parkir_jurnal;
 			//bensin_tol_parkir
 
+			//pakaian_dinas
+			$pakaian_dinas_biaya = $this->db->select('pdb.*, pb.id as biaya_id, pb.tanggal_transaksi, pb.nomor_transaksi')
+			->from('pmm_biaya pb ')
+			->join('pmm_detail_biaya pdb','pb.id = pdb.biaya_id','left')
+			->join('pmm_coa c','pdb.akun = c.id','left')
+			->where('c.id',138)
+			->where("pb.status = 'PAID'")
+			->where("(pb.tanggal_transaksi between '$date1' and '$date2')")
+			->group_by('pdb.id')
+			->get()->result_array();
+
+			$total_debit_pakaian_dinas_biaya = 0;
+			foreach ($pakaian_dinas_biaya as $x){
+				$total_debit_pakaian_dinas_biaya += $x['jumlah'];
+			}
+
+			$pakaian_dinas_jurnal = $this->db->select('pdb.*, pb.id as jurnal_id, pb.tanggal_transaksi, pb.nomor_transaksi, pb.total_debit, pb.total_kredit')
+			->from('pmm_jurnal_umum pb ')
+			->join('pmm_detail_jurnal pdb','pb.id = pdb.jurnal_id','left')
+			->join('pmm_coa c','pdb.akun = c.id','left')
+			->where('c.id',138)
+			->where("(pb.tanggal_transaksi between '$date1' and '$date2')")
+			->group_by('pdb.id')
+			->get()->result_array();
+
+			$total_debit_pakaian_dinas_jurnal = 0;
+			$total_kredit_pakaian_dinas_jurnal = 0;
+
+			foreach ($pakaian_dinas_jurnal as $x){
+				$total_debit_pakaian_dinas_jurnal += $x['total_debit'];
+				$total_kredit_pakaian_dinas_jurnal += $x['total_kredit'];
+			}
+			
+			$total_debit_pakaian_dinas_all = 0;
+			$total_kredit_pakaian_dinas_all = 0;
+			$total_debit_pakaian_dinas_all = $total_debit_pakaian_dinas_biaya + $total_debit_pakaian_dinas_jurnal;
+			$total_kredit_pakaian_dinas_all = $total_kredit_pakaian_dinas_jurnal;
+			//pakaian_dinas
+
 	        ?>
 
 			<tr class="table-active">
@@ -4342,6 +4381,48 @@ class Reports extends CI_Controller {
 			?>
 	    </table>
 		<!-- bensin_tol_parkir -->
+
+		<!-- pakaian_dinas -->
+		<table class="table table-bordered" width="100%">
+			<tr class="table-active">
+	            <th width="40%" class="text-left"><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#pakaian_dinas" aria-expanded="false" aria-controls="pakaian_dinas">(5-50508) Bensin Tol dan Parkir - Umum</button></th>
+				<th width="20%" class="text-center"><?php echo number_format($total_debit_pakaian_dinas_all,0,',','.');?></th>
+				<th width="20%" class="text-center"><?php echo number_format($total_kredit_pakaian_dinas_all,0,',','.');?></th>
+				<th width="20%" class="text-center"></th>
+	        </tr>
+		</table>	
+		<table class="collapse table table-bordered" id="pakaian_dinas" width="100%">
+			<tr class="table-active" width="100%">
+				<th width="10%" class="text-center">Tanggal</th>
+				<th width="10%" class="text-center">Transaksi</th>
+				<th width="30%" class="text-center">Nomor</th>
+				<th width="50%" class="text-center">Keterangan</th>
+	        </tr>
+			<?php foreach ($pakaian_dinas_biaya as $key => $x) {
+			?>
+			<tr class="table-active3">
+				<th class="text-center"><?= date('d-m-Y',strtotime($x['tanggal_transaksi'])); ?></th>
+				<th class="text-center">TRANSAKSI BIAYA</th>
+				<th class="text-left"><a target="_blank" href="<?= base_url("pmm/biaya/detail_biaya/".$x['biaya_id']) ?>"><?= $x['nomor_transaksi'] ?></a></th>
+				<th class="text-left"><?= $x['deskripsi'] ?></th>
+	        </tr>
+			<?php
+			}
+			?>
+
+			<?php foreach ($pakaian_dinas_jurnal as $key => $x) {
+			?>
+			<tr class="table-active3">
+				<th class="text-center"><?= date('d-m-Y',strtotime($x['tanggal_transaksi'])); ?></th>
+				<th class="text-center">JURNAL UMUM</th>
+				<th class="text-left"><a target="_blank" href="<?= base_url("pmm/jurnal_umum/detailJurnal/".$x['jurnal_id']) ?>"><?= $x['nomor_transaksi'] ?></a></th>
+				<th class="text-left"><?= $x['deskripsi'] ?></th>
+	        </tr>
+			<?php
+			}
+			?>
+	    </table>
+		<!-- pakaian_dinas -->
 
 
 		
