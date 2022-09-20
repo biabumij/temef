24d8fc710c23565bddfaf8c7fabc2e948eb82ffe
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-09-20 15:19:29 +0700

    update

--- application/modules/pmm/controllers/Reports.php
+++ application/modules/pmm/controllers/Reports.php
@@ -2356,7 +2356,7 @@ class Reports extends CI_Controller {
 									<span>Rp.</span>
 								</th>
 								<th class="text-right" width="90%">
-									<span><a target="_blank" href="<?= base_url("laporan/cetak_diskonto?filter_date=".$filter_date_2 = date('d F Y',strtotime($date3)).' - '.date('d F Y',strtotime($arr_filter_date[1]))) ?>"><?php echo number_format($persiapan_2,0,',','.');?></a></span>
+									<span><a target="_blank" href="<?= base_url("laporan/cetak_persiapan?filter_date=".$filter_date_2 = date('d F Y',strtotime($date3)).' - '.date('d F Y',strtotime($arr_filter_date[1]))) ?>"><?php echo number_format($persiapan_2,0,',','.');?></a></span>
 								</th>
 							</tr>
 					</table>
