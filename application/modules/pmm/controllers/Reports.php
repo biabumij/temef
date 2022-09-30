ac7b60e2d28711cdb7384d2ee0b923817ad57ebb
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-09-30 20:23:53 +0700

    update

--- application/modules/pmm/controllers/Reports.php
+++ application/modules/pmm/controllers/Reports.php
@@ -6808,15 +6808,16 @@ class Reports extends CI_Controller {
 				<th width="5%" class="text-center" rowspan="2" style="vertical-align:middle">NO.</th>
 				<th width="15%" class="text-center" rowspan="2" style="vertical-align:middle">URAIAN</th>
 				<th width="10%" class="text-center" rowspan="2" style="vertical-align:middle">SATUAN</th>
-				<th width="15%" class="text-center" rowspan="2" style="vertical-align:middle">REALISASI SMT 1-2 SD AGUSTUS 2022</th>
+				<th width="15%" class="text-center" rowspan="2" style="vertical-align:middle">REALISASI AGUSTUS 2022</th>
 				<th width="40%" class="text-center" colspan="4">2022 (SMT 3)</th>
 				<th width="15%" class="text-center">TOTAL SD. SMT3</th>
 	        </tr>
 			<tr class="table-active4">
+				<th class="text-center">SEPTEMBER</th>
 				<th class="text-center">SD. SEPTEMBER</th>
-				<th class="text-center">SD. OKTOBER</th>
-				<th class="text-center">SD. NOVEMBER</th>
-				<th class="text-center">SD. DESEMBER</th>
+				<th class="text-center">OKTOBER</th>
+				<th class="text-center">NOVEMBER</th>
+				<th class="text-center">DESEMBER</th>
 				<th class="text-center">SD. 2022</th>
 	        </tr>
 			<tr class="table-active3">
