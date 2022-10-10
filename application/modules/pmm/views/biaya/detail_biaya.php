c6838364fbd427439b6fdfe7a48cb7d8fadbfb92
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-10-10 10:09:14 +0700

    update

--- application/modules/pmm/views/biaya/detail_biaya.php
+++ application/modules/pmm/views/biaya/detail_biaya.php
@@ -136,7 +136,7 @@
                                             if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 10 || $this->session->userdata('admin_group_id') == 13|| $this->session->userdata('admin_group_id') == 14 || $this->session->userdata('admin_group_id') == 19){
                                             ?>
                                             <!--<a class="btn btn-danger" onclick="DeleteData('<?= site_url('pmm/biaya/delete/'.$row['id']);?>')"><i class="fa fa-close"></i> Hapus</a>-->
-                                            <a  href="<?= base_url('pmm/biaya/form/'.$row['id']) ?>" class="btn btn-primary"><i class="fa fa-edit"></i> Edit</a>
+                                            <!--<a  href="<?= base_url('pmm/biaya/form/'.$row['id']) ?>" class="btn btn-primary"><i class="fa fa-edit"></i> Edit</a>-->
                                             <?php
                                             }
                                             ?>
