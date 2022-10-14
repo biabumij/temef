7648308f9b8e44c30fe1f4e2ef023995f9c1ed87
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-10-14 09:47:33 +0700

    update

--- application/modules/admin/views/form_edit.php
+++ application/modules/admin/views/form_edit.php
@@ -27,7 +27,7 @@
                             <h3 class="section-subtitle">Edit Profile</h3>
                         </div>
                         <div class="panel-content">
-                            <form id="inline-validation" method="POST" class="form-horizontal form-stripe form-submit" novalidate="novalidate" data-button="#btn-submit" action="<?php echo site_url('admin/edit');?>" data-redirect="<?php echo site_url('admin/admin');?>">
+                            <form id="inline-validation" method="POST" class="form-horizontal form-stripe form-submit" novalidate="novalidate" data-button="#btn-submit" action="<?php echo site_url('admin/edit');?>" data-redirect="<?php echo site_url('admin/dashboard');?>">
                                 <input type="hidden" name="id" value="<?php echo $row[0]->admin_id;?>">
                                 <div class="form-group">
                                     <label for="name" class="col-sm-2 control-label">Name<span class="required" aria-required="true">*</span></label>
