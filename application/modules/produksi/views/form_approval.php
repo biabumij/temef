7008a0edd1121c950b89ff1b7e0bc2a8096c4b57
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-10-26 16:54:07 +0700

    update

--- application/modules/produksi/views/form_approval.php
+++ application/modules/produksi/views/form_approval.php
@@ -41,7 +41,7 @@
                                 <form method="POST" action="<?php echo site_url('produksi/submit_approval');?>" id="form-po" enctype="multipart/form-data" autocomplete="off">
                                     <div class="row">
                                         <div class="col-sm-2">
-                                            <label>Tanggal Akumulasi</label>
+                                            <label>Tanggal Persetujuan</label>
                                         </div>
 										 <div class="col-sm-2">
                                             <input type="text" class="form-control dtpicker" name="date_approval" required="" value="" />
