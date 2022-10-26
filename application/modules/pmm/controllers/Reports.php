4ab6891da61f88531cc59c839987191531983f4d
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-10-26 15:44:51 +0700

    update

--- application/modules/pmm/controllers/Reports.php
+++ application/modules/pmm/controllers/Reports.php
@@ -2852,45 +2852,45 @@ class Reports extends CI_Controller {
 			<style type="text/css">
 				table tr.table-active{
 					background-color: #F0F0F0;
-					font-size: 8px;
+					font-size: 12px;
 					font-weight: bold;
 					color: black;
 				}
 					
 				table tr.table-active2{
 					background-color: #E8E8E8;
-					font-size: 8px;
+					font-size: 12px;
 					font-weight: bold;
 				}
 					
 				table tr.table-active3{
-					font-size: 8px;
+					font-size: 12px;
 					background-color: #F0F0F0;
 				}
 					
 				table tr.table-active4{
 					background-color: #e69500;
 					font-weight: bold;
-					font-size: 8px;
+					font-size: 12px;
 					color: black;
 				}
 				table tr.table-active5{
 					background-color: #E8E8E8;
 					text-decoration: underline;
-					font-size: 8px;
+					font-size: 12px;
 					font-weight: bold;
 					color: red;
 				}
 				table tr.table-activeago1{
 					background-color: #ffd966;
 					font-weight: bold;
-					font-size: 8px;
+					font-size: 12px;
 					color: black;
 				}
 				table tr.table-activeopening{
 					background-color: #2986cc;
 					font-weight: bold;
-					font-size: 8px;
+					font-size: 12px;
 					color: black;
 				}
 			</style>
@@ -9666,45 +9666,45 @@ class Reports extends CI_Controller {
 			<style type="text/css">
 				table tr.table-active{
 					background-color: #F0F0F0;
-					font-size: 8px;
+					font-size: 12px;
 					font-weight: bold;
 					color: black;
 				}
 					
 				table tr.table-active2{
 					background-color: #E8E8E8;
-					font-size: 8px;
+					font-size: 12px;
 					font-weight: bold;
 				}
 					
 				table tr.table-active3{
-					font-size: 8px;
+					font-size: 12px;
 					background-color: #F0F0F0;
 				}
 					
 				table tr.table-active4{
 					background-color: #e69500;
 					font-weight: bold;
-					font-size: 8px;
+					font-size: 12px;
 					color: black;
 				}
 				table tr.table-active5{
 					background-color: #E8E8E8;
 					text-decoration: underline;
-					font-size: 8px;
+					font-size: 12px;
 					font-weight: bold;
 					color: red;
 				}
 				table tr.table-activeago1{
 					background-color: #ffd966;
 					font-weight: bold;
-					font-size: 8px;
+					font-size: 12px;
 					color: black;
 				}
 				table tr.table-activeopening{
 					background-color: #2986cc;
 					font-weight: bold;
-					font-size: 8px;
+					font-size: 12px;
 					color: black;
 				}
 			</style>
