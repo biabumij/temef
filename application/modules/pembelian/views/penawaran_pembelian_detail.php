2af0edc84c7667e2236356a2645c05ec491e958d
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-11-11 14:45:49 +0700

    update

--- application/modules/pembelian/views/penawaran_pembelian_detail.php
+++ application/modules/pembelian/views/penawaran_pembelian_detail.php
@@ -135,7 +135,7 @@
 								</table>    
                                     
                                     <div class="row">
-                                        <div class="col-sm-16">
+                                        <div class="col-sm-12">
                                             <div class="form-group">
                                                 <label>Memo :</label>
                                                 <td><?= $row["memo"]; ?></td>
