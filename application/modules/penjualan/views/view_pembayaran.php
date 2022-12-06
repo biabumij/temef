cb994c6aa9142aefefd012fe31b521da65ef9a4e
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-12-06 09:09:35 +0700

    update

--- application/modules/penjualan/views/view_pembayaran.php
+++ application/modules/penjualan/views/view_pembayaran.php
@@ -136,7 +136,7 @@
                                             <a href="<?= base_url('penjualan/detailPenagihan/' . $bayar["penagihan_id"]) ?>" class="btn btn-info" style="margin-bottom:0;"><i class="fa fa-mail-replay"></i> Kembali</a>
                                             <a href="<?= base_url('penjualan/cetak_pembayaran/' . $bayar["id"]) ?>" target="_blank" class="btn btn-info" style="margin-bottom:0;"><i class="fa fa-print"></i> Cetak PDF</a>
                                             <?php
-                                            if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 10 || $this->session->userdata('admin_group_id') == 19){
+                                            if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 10 || $this->session->userdata('admin_group_id') == 13 || $this->session->userdata('admin_group_id') == 14 || $this->session->userdata('admin_group_id') == 19){
                                             ?>
                                             <a href="<?= base_url('penjualan/sunting_pembayaran/' . $bayar["id"]) ?>" class="btn btn-warning" style="margin-bottom:0;"><i class="fa fa-check"></i> Edit</a>
                                             <?php
