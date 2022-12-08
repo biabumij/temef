6ce8df0186c4b4246d1b3917c276dd601098d49c
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-12-08 14:11:42 +0700

    update

--- application/modules/rak_cash_flow/views/index.php
+++ application/modules/rak_cash_flow/views/index.php
@@ -62,6 +62,11 @@
                                                     <tr>
                                                         <th class="text-center" width="5%">No.</th>
 														<th class="text-center">Tanggal</th>
+                                                        <th class="text-center">Biaya Bahan</th>
+                                                        <th class="text-center">Biaya Alat</th>
+														<th class="text-center">Biaya Overhead</th>
+                                                        <th class="text-center">Biaya Bank</th>
+                                                        <th class="text-center">Termin</th>
                                                         <th class="text-center">Lampiran</th>
                                                         <th class="text-center">Dibuat Oleh</th>
                                                         <th class="text-center">Dibuat Tanggal</th>
@@ -126,6 +131,21 @@
                     "data": "tanggal_rencana_kerja"
                 },
                 {
+                    "data": "biaya_bahan"
+                },
+                {
+                    "data": "biaya_alat"
+                },
+				{
+                    "data": "biaya_overhead"
+                },
+                {
+                    "data": "biaya_bank"
+                },
+                {
+                    "data": "termin"
+                },
+                {
                     "data": "lampiran"
                 },
                 {
@@ -145,9 +165,13 @@
 				},
             ],
             "columnDefs": [{
-                    "targets": [0, 3, 4, 5, 6, 7],
+                    "targets": [0, 1, 8, 9, 10, 11, 12],
                     "className": 'text-center',
-                }
+                },
+                {
+                "targets": [2, 3, 4, 5, 6],
+                "className": 'text-right',
+                },
             ],
         });
 
