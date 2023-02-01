c2f79c4059cb714533462f9554dde03d834a4a60
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-02-01 13:10:28 +0700

    update

--- application/modules/pembelian/views/index.php
+++ application/modules/pembelian/views/index.php
@@ -257,7 +257,7 @@
                                                     <th class="text-center">Terima</th>
                                                     <th class="text-center">Total Pesanan Pembelian</th>
                                                     <th class="text-center">Total Terima</th>
-                                                    <th class="text-center">Status PO</th>
+                                                    <th class="text-center">Status Pesanan Pembelian</th>
                                                     <th class="text-center">Lampiran</th>
                                                     <th class="text-center">Tindakan</th>
                                                     <th class="text-center">Dibuat Oleh</th>
