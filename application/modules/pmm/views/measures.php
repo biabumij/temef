882dfd7fd882df0573dc04af666cad55341770b1
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-02-22 10:03:47 +0700

    update

--- application/modules/pmm/views/measures.php
+++ application/modules/pmm/views/measures.php
@@ -108,66 +108,6 @@
         </div>
     </div>
 
-    <div class="modal fade bd-example-modal-lg" id="modalDetail" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg" role="document" >
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="modal-title">Tool Detail</span>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="<?php echo site_url('pmm/measure_convert_form');?>" method="POST" id="form-man">
-                        <input type="hidden" name="sub_detail_id" id="sub_detail_id">
-                        <input type="hidden" id="detail_id" name="detail_id">
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <select id="measure_to" name="measure_to" class="form-control">
-                                    <option value="">.. Select Measures ..</option>
-                                    <?php
-                                    $measures = $this->db->get_where('pmm_measures',array('status !='=>'DELETED'))->result_array();
-                                    foreach ($measures as $key => $to) {
-                                        ?>
-                                        <option value="<?php echo $to['id'];?>"><?php echo $to['measure_name'];?></option>
-                                        <?php
-                                    }
-                                    ?>  
-                                </select>
-                            </div>
-                            <div class="col-sm-3">
-                                <input type="text"  id="value" name="value" class="form-control numberformat" autocomplete="off" placeholder="Value Convert" required="">
-                            </div>
-                            <div class="col-sm-3">
-                                <button type="submit" name="submit" id="btn-man" class="btn btn-warning"><i class="fa fa-save"></i> Simpan</button>
-                                <button type="button" id="btn-unedit" class="btn btn-info"><i class="fa fa-undo"></i></button>
-                            </div>
-                        </div>
-                    </form>
-                    <br />
-                    <div class="table-responsive">
-                        <table class="table table-striped table-hover table-center" id="detail-table" width="100%">
-                            <thead>
-                                <tr>
-                                    <th width="5%">No</th>
-                                    <th>Satuan</th>
-                                    <th>Nilai</th>
-                                    <th>Tindakan</th>zz
-                                </tr>
-                            </thead>
-                            <tbody>
-                               
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
 	<script src="<?php echo base_url();?>assets/back/theme/vendor/bootbox.min.js"></script>
     <script src="<?php echo base_url();?>assets/back/theme/vendor/jquery.number.min.js"></script>
     <script type="text/javascript">
@@ -194,36 +134,6 @@
                 }
             ],
             responsive: true,
-        });
-
-        var table_detail = $('#detail-table').DataTable( {
-            ajax: {
-                processing: true,
-                serverSide: true,
-                url: '<?php echo site_url('pmm/table_measure_convert');?>',
-                type : 'POST',
-                data: function ( d ) {
-                    d.detail_id = $('#detail_id').val();
-                }
-            },
-            columns: [
-                { "data": "no" },
-                { "data": "name" },
-                { "data": "value" },
-                { "data": "actions" }
-            ],
-            "columnDefs": [
-                {
-                    "targets": [0],
-                    "className": 'text-center',
-                    "width" : '10px'
-                },
-                {
-                    "targets": [-1],
-                    "className": 'text-center',
-                    "width" : '150px'
-                }
-            ],
         });
 
         function OpenForm(id='')
@@ -303,88 +213,6 @@
                 }
             });
         }
-
-        function FormDetail(id,name)
-        {   
-            $('#detail_id').val(id);
-            $('#detail_product_id').val('');
-            $("#modalDetail form").trigger("reset");
-            $('#modalDetail').modal('show');
-            $('#modalDetail .modal-title').text(name);
-            table_detail.ajax.reload();
-            
-        }
-
-        $('#form-man').on('submit',function(event){
-            var form = $(this);
-
-            var formAction = form.attr('action');
-            $('#btn-man').button('loading');
-            $.ajax({
-                url         : formAction,
-                data        : form.serialize(),
-                type        : 'POST',
-                dataType    : 'json',
-                success     : function(data){
-                    // $('#loader').hide();
-                    $('#btn-man').button('reset');
-                    // Callback code
-                    if(data.output){
-                        $("#form-man").trigger("reset");
-                        table_detail.ajax.reload();
-                        table.ajax.reload();
-                        $('#detail_product_id').val('');
-                        $('#btn-unedit').hide();
-                    }else if(data.err){
-                        bootbox.alert(data.err);
-                    }
-                }
-            });
-            
-
-            event.preventDefault();
-        });
-
-        function EditDetail(id,measure_to,value)
-        {
-           $('#sub_detail_id').val(id);
-           $('#measure_to').val(measure_to);
-           $('#value').val(value);
-           $('#btn-unedit').show();
-
-        }
-
-        $('#btn-unedit').click(function(){
-            $("#form-man").trigger("reset");
-            $('#sub_detail_id').val('');
-            $(this).hide();
-        });
-
-        function DeleteDataDetail(id)
-        {
-            bootbox.confirm("Are you sure to delete this data ?", function(result){ 
-                // console.log('This was logged in the callback: ' + result); 
-                if(result){
-                    $.ajax({
-                        type    : "POST",
-                        url     : "<?php echo site_url('pmm/delete_measure_convert'); ?>",
-                        dataType : 'json',
-                        data: {id:id},
-                        success : function(result){
-                            if(result.output){
-                                table_detail.ajax.reload();
-                                bootbox.alert('Berhasil menghapus!!');
-                            }else if(result.err){
-                                bootbox.alert(result.err);
-                            }
-                        }
-                    });
-                }
-            });
-        }
-
-
-
 
     </script>
 
