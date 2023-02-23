67e35720bb1efb7d39cc061abc348b8a4c4f9965
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-02-23 11:24:51 +0700

    update

--- application/modules/admin/models/Templates.php
+++ application/modules/admin/models/Templates.php
@@ -10,17 +10,11 @@ class Templates extends CI_Model {
 
     function Header()
     {
-		$active = false;
-		if($selected == 0){
-			$active = $this->uri->segment(2);
-		}else {
-			$active = false;
-		}		
+		$jenis_usaha = $this->crud_global->GetField('pmm_setting_production',array('id'=>1),'jenis_usaha');
     	?>
     	<meta charset="UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	    <!--<title><?php echo $this->m_themes->GetThemes('site_name');?></title>-->
-		<title><?php echo strtoupper($active);?></title>
+	    <title><?= $jenis_usaha;?></title>
 	    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url().$this->m_themes->GetThemes('site_favico');?>">
 	    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo base_url().$this->m_themes->GetThemes('site_favico');?>">
 	    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url().$this->m_themes->GetThemes('site_favico');?>">
