cc5eacc4fffa6ad9eadf812b13ced7d46e1cd2d0
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-03-30 11:27:40 +0700

    update

--- application/modules/rak/controllers/Rak.php
+++ application/modules/rak/controllers/Rak.php
@@ -729,7 +729,7 @@ class Rak extends Secure_Controller {
 			$data['products'] =  $this->db->select('*')
 			->from('produk p')
 			->where("p.status = 'PUBLISH'")
-			->where("p.betonreadymix = 1 ")
+			->where("p.kategori_produk = 1 ")
 			->order_by('nama_produk','asc')
 			->get()->result_array();
 			$this->load->view('rak/form_rencana_cash_flow', $data);
