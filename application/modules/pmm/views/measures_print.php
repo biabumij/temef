76e0f3a50d648c0fdb5e131c2a2f294c006bc496
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-03-31 14:19:13 +0700

    update

--- application/modules/pmm/views/measures_print.php
+++ application/modules/pmm/views/measures_print.php
@@ -4,43 +4,91 @@
 	  <title>SATUAN</title>
 	  
 	  <style type="text/css">
-	  	body{
-	  		font-family: "Open Sans", Arial, sans-serif;
-	  	}
-	  	table.minimalistBlack {
-		  border: 0px solid #000000;
-		  width: 100%;
-		  text-align: left;
+		 body {
+			font-family: helvetica;
 		}
-		table.minimalistBlack td, table.minimalistBlack th {
-		  border: 1px solid #000000;
-		  /*padding: 10px 4px;*/
+
+		table.table-border-pojok-kiri, th.table-border-pojok-kiri, td.table-border-pojok-kiri {
+			border-top: 1px solid black;
+			border-bottom: 1px solid black;
+			border-right: 1px solid #cccccc;
+			border-left: 1px solid black;
 		}
-		table.minimalistBlack tr th {
-		  /*font-size: 14px;*/
-		  font-weight: bold;
-		  color: #000000;
-		  text-align: center;
+
+		table.table-border-pojok-tengah, th.table-border-pojok-tengah, td.table-border-pojok-tengah {
+			border-top: 1px solid black;
+			border-bottom: 1px solid black;
+			border-right: 1px solid #cccccc;
 		}
-		table tr.table-active{
-            background-color: #b5b5b5;
-        }
-        table tr.table-active2{
-            background-color: #cac8c8;
-        }
-        
-		hr{
-			margin-top:0;
-			margin-bottom:30px;
+
+		table.table-border-pojok-kanan, th.table-border-pojok-kanan, td.table-border-pojok-kanan {
+			border-top: 1px solid black;
+			border-bottom: 1px solid black;
+			border-right: 1px solid black;
 		}
-		h3{
-			margin-top:0;
+
+		table.table-border-spesial, th.table-border-spesial, td.table-border-spesial {
+			border-left: 1px solid black;
+			border-right: 1px solid black;
+		}
+
+		table.table-border-spesial-kiri, th.table-border-spesial-kiri, td.table-border-spesial-kiri {
+			border-left: 1px solid black;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+		}
+
+		table.table-border-spesial-tengah, th.table-border-spesial-tengah, td.table-border-spesial-tengah {
+			border-left: 1px solid #cccccc;
+			border-right: 1px solid #cccccc;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+		}
+
+		table.table-border-spesial-kanan, th.table-border-spesial-kanan, td.table-border-spesial-kanan {
+			border-left: 1px solid #cccccc;
+			border-right: 1px solid black;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+		}
+
+		table tr.table-judul{
+			border: 1px solid;
+			background-color: #e69500;
+			font-weight: bold;
+			font-size: 7px;
+			color: black;
+		}
+			
+		table tr.table-baris1{
+			background-color: none;
+			font-size: 7px;
+		}
+
+		table tr.table-baris1-bold{
+			background-color: none;
+			font-size: 7px;
+			font-weight: bold;
+		}
+			
+		table tr.table-total{
+			background-color: #FFFF00;
+			font-weight: bold;
+			font-size: 7px;
+			color: black;
+		}
+
+		table tr.table-total2{
+			background-color: #eeeeee;
+			font-weight: bold;
+			font-size: 7px;
+			color: black;
 		}
 	  </style>
 
 	</head>
 	<body>
-		<table width="98%" border="0" cellpadding="3">
+		<table width="98%" cellpadding="3">
 			<tr>
 				<td align="center">
 					<div style="display: block;font-weight: bold;font-size: 11px;">SATUAN</div>
@@ -50,11 +98,11 @@
 		<br />
 		<br />
 		<br />
-		<table class="minimalistBlack" cellpadding="2" width="98%">
-			<tr class="table-active">
-                <th width="5%">No.</th>
-                <th width="75%">Satuan</th>
-                <th width="20%">Status</th>
+		<table cellpadding="3" width="98%">
+			<tr class="table-judul">
+                <th width="5%" align="center" class="table-border-pojok-kiri">NO.</th>
+                <th width="75%" align="center" class="table-border-pojok-tengah">SATUAN</th>
+                <th width="20%" align="center" class="table-border-pojok-kanan">STATUS</th>
             </tr>
             <?php
             
@@ -66,9 +114,9 @@
 
             		?>
             		<tr>
-            			<td align="center"><?php echo $no;?></td>
-            			<td><?php echo $row['measure_name'];?></td>
-            			<td align="center"><?php echo $row['status'];?></td>
+            			<td align="center" class="table-border-pojok-kiri"><?php echo $no;?>.</td>
+            			<td class="table-border-pojok-tengah"><?php echo $row['measure_name'];?></td>
+            			<td align="center" class="table-border-pojok-kanan"><?php echo $row['status'];?></td>
             		</tr>
             		<?php	
             		$no++;
@@ -78,7 +126,7 @@
             }else {
             	?>
             	<tr>
-            		<td width="100%" colspan="8" align="center">Tidak Ada Data</td>
+            		<td width="100%" colspan="3" align="center">Tidak Ada Data</td>
             	</tr>
             	<?php
             }
--- application/modules/rak/views/cetak_rencana_cash_flow.php
+++ application/modules/rak/views/cetak_rencana_cash_flow.php
@@ -4,56 +4,94 @@
 	  <?= include 'lib.php'; ?>
 
 	  <style type="text/css">
+		 body {
+			font-family: helvetica;
+		}
+
+		table.table-border-pojok-kiri, th.table-border-pojok-kiri, td.table-border-pojok-kiri {
+			border-top: 1px solid black;
+			border-bottom: 1px solid black;
+			border-right: 1px solid #cccccc;
+			border-left: 1px solid black;
+		}
+
+		table.table-border-pojok-tengah, th.table-border-pojok-tengah, td.table-border-pojok-tengah {
+			border-top: 1px solid black;
+			border-bottom: 1px solid black;
+			border-right: 1px solid #cccccc;
+		}
+
+		table.table-border-pojok-kanan, th.table-border-pojok-kanan, td.table-border-pojok-kanan {
+			border-top: 1px solid black;
+			border-bottom: 1px solid black;
+			border-right: 1px solid black;
+		}
+
+		table.table-border-spesial, th.table-border-spesial, td.table-border-spesial {
+			border-left: 1px solid black;
+			border-right: 1px solid black;
+		}
+
+		table.table-border-spesial-kiri, th.table-border-spesial-kiri, td.table-border-spesial-kiri {
+			border-left: 1px solid black;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+		}
+
+		table.table-border-spesial-tengah, th.table-border-spesial-tengah, td.table-border-spesial-tengah {
+			border-left: 1px solid #cccccc;
+			border-right: 1px solid #cccccc;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+		}
+
+		table.table-border-spesial-kanan, th.table-border-spesial-kanan, td.table-border-spesial-kanan {
+			border-left: 1px solid #cccccc;
+			border-right: 1px solid black;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+		}
+
 		table tr.table-judul{
+			border: 1px solid;
 			background-color: #e69500;
 			font-weight: bold;
-			font-size: 8px;
+			font-size: 7px;
 			color: black;
 		}
 			
 		table tr.table-baris1{
-			background-color: #F0F0F0;
-			font-size: 8px;
+			background-color: none;
+			font-size: 7px;
 		}
 
 		table tr.table-baris1-bold{
-			background-color: #F0F0F0;
-			font-size: 8px;
-			font-weight: bold;
-		}
-			
-		table tr.table-baris2{
-			font-size: 8px;
-			background-color: #E8E8E8;
-		}
-
-		table tr.table-baris2-bold{
-			font-size: 8px;
-			background-color: #E8E8E8;
+			background-color: none;
+			font-size: 7px;
 			font-weight: bold;
 		}
 			
 		table tr.table-total{
-			background-color: #cccccc;
+			background-color: #FFFF00;
 			font-weight: bold;
-			font-size: 8px;
+			font-size: 7px;
+			color: black;
+		}
+
+		table tr.table-total2{
+			background-color: #eeeeee;
+			font-weight: bold;
+			font-size: 7px;
 			color: black;
 		}
 	  </style>
 
 	</head>
 	<body>
-		<table width="100%" border="0" cellpadding="3">
-			<tr>
-				<td align="center">
-					<div style="display: block;font-weight: bold;font-size: 12px;">RENCANA CASH FLOW<br/>
-					DIVISI BETON PROYEK BENDUNGAN TEMEF<br/>
-					PT. BIA BUMI JAYENDRA<br/></div>
-				</td>
-			</tr>
-		</table>
-		<br />
-		<br />
+		<div align="center" style="display: block;font-weight: bold;font-size: 12px;">RENCANA CASH FLOW<br/>
+		DIVISI BETON PROYEK BENDUNGAN TEMEF<br/>
+		PT. BIA BUMI JAYENDRA<br/></div>
+		<br /><br /><br />
 		<table width="100%" border="0">
 			<?php
 			$tanggal = $rak['tanggal_rencana_kerja'];
@@ -95,39 +133,39 @@
 		<br />
 		<table cellpadding="5" width="98%">
 			<tr class="table-judul">
-                <th width="5%" align="center">NO.</th>
-                <th width="35%" align="center">URAIAN</th>
-				<th width="60%" align="right">NILAI</th>
+                <th width="5%" align="center" class="table-border-pojok-kiri">NO.</th>
+                <th width="35%" align="center" class="table-border-pojok-tengah">URAIAN</th>
+				<th width="60%" align="right" class="table-border-pojok-kanan">NILAI</th>
             </tr>
 			<tr class="table-baris1">
-				<td align="center">1.</td>
-				<td align="left">Biaya Bahan</td>
-				<td align="right"><?= number_format($rak['biaya_bahan'],0,',','.'); ?></td>
+				<td align="center" class="table-border-pojok-kiri">1.</td>
+				<td align="left" class="table-border-pojok-tengah">Biaya Bahan</td>
+				<td align="right" class="table-border-pojok-kanan"><?= number_format($rak['biaya_bahan'],0,',','.'); ?></td>
 			</tr>
 			<tr class="table-baris1">
-				<td align="center">2.</td>
-				<td align="left">Biaya Alat</td>
-				<td align="right"><?= number_format($rak['biaya_alat'],0,',','.'); ?></td>
+				<td align="center" class="table-border-pojok-kiri">2.</td>
+				<td align="left" class="table-border-pojok-tengah">Biaya Alat</td>
+				<td align="right" class="table-border-pojok-kanan"><?= number_format($rak['biaya_alat'],0,',','.'); ?></td>
 			</tr>
 			<tr class="table-baris1">
-				<td align="center">3.</td>
-				<td align="left">Biaya Bank</td>
-				<td align="right"><?= number_format($rak['biaya_bank'],0,',','.'); ?></td>
+				<td align="center" class="table-border-pojok-kiri">3.</td>
+				<td align="left" class="table-border-pojok-tengah">Biaya Bank</td>
+				<td align="right" class="table-border-pojok-kanan"><?= number_format($rak['biaya_bank'],0,',','.'); ?></td>
 			</tr>
 			<tr class="table-baris1">
-				<td align="center">4.</td>
-				<td align="left">Overhead</td>
-				<td align="right"><?= number_format($rak['overhead'],0,',','.'); ?></td>
+				<td align="center" class="table-border-pojok-kiri">4.</td>
+				<td align="left" class="table-border-pojok-tengah">Overhead</td>
+				<td align="right" class="table-border-pojok-kanan"><?= number_format($rak['overhead'],0,',','.'); ?></td>
 			</tr>
 			<tr class="table-baris1">
-				<td align="center">5.</td>
-				<td align="left">Termin</td>
-				<td align="right"><?= number_format($rak['termin'],0,',','.'); ?></td>
+				<td align="center" class="table-border-pojok-kiri">5.</td>
+				<td align="left" class="table-border-pojok-tengah">Termin</td>
+				<td align="right" class="table-border-pojok-kanan"><?= number_format($rak['termin'],0,',','.'); ?></td>
 			</tr>
 			<tr class="table-baris1">
-				<td align="center">6.</td>
-				<td align="left">Biaya Persiapan</td>
-				<td align="right"><?= number_format($rak['biaya_persiapan'],0,',','.'); ?></td>
+				<td align="center" class="table-border-pojok-kiri">6.</td>
+				<td align="left" class="table-border-pojok-tengah">Biaya Persiapan</td>
+				<td align="right" class="table-border-pojok-kanan"><?= number_format($rak['biaya_persiapan'],0,',','.'); ?></td>
 			</tr>
 		</table>
 	</body>
