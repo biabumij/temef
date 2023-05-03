441beee1215aad9aca8997c828d5b3a82ec8befd
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-05-03 14:28:27 +0700

    update

--- application/modules/pmm/controllers/Receipt_material.php
+++ application/modules/pmm/controllers/Receipt_material.php
@@ -279,11 +279,11 @@ class Receipt_material extends CI_Controller {
 
 				$row['status_payment'] = $this->pmm_model->StatusPayment($row['status_payment']);
 				$edit = false;
-				if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 4){
+				if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 4 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 11 || $this->session->userdata('admin_group_id') == 16){
 					$edit = '<a href="javascript:void(0);" onclick="EditData('.$row['id'].')" class="btn btn-primary"><i class="fa fa-edit"></i> </a>';			
 				}
 
-				if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 4 || $this->session->userdata('admin_group_id') == 11  || $this->session->userdata('admin_group_id') == 15){
+				if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 4 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 11 || $this->session->userdata('admin_group_id') == 16){
 					$row['actions'] = $edit. ' <a href="javascript:void(0);" onclick="DeleteData('.$row['id'].')" class="btn btn-danger"><i class="fa fa-close"></i> </a>';
 				}else {
 					$row['actions'] = '-';
