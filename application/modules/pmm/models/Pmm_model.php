6bcc56935f7e3aac043a209b326194ef4725a8a8
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-06-19 15:24:13 +0700

    update

--- application/modules/pmm/models/Pmm_model.php
+++ application/modules/pmm/models/Pmm_model.php
@@ -3966,8 +3966,8 @@ class Pmm_model extends CI_Model {
         $this->db->join('pmm_verifikasi_penagihan_pembelian pvp','ppp.id = pvp.penagihan_pembelian_id','left');
         $this->db->join('pmm_purchase_order ppo','ppp.purchase_order_id = ppo.id','left');
         if(!empty($start_date) && !empty($end_date)){
-            $this->db->where('ppp.created_on >=',$start_date);
-            $this->db->where('ppp.created_on <=',$end_date);
+            $this->db->where('ppp.tanggal_invoice >=',$start_date);
+            $this->db->where('ppp.tanggal_invoice <=',$end_date);
         }
         if(!empty($supplier_id)){
             $this->db->where('ppp.supplier_id',$supplier_id);
@@ -4009,8 +4009,8 @@ class Pmm_model extends CI_Model {
         $this->db->join('pmm_purchase_order ppo','ppp.purchase_order_id = ppo.id','left');
         $this->db->where("ppo.kategori_id in (1,5)");
         if(!empty($start_date) && !empty($end_date)){
-            $this->db->where('ppp.created_on >=',$start_date);
-            $this->db->where('ppp.created_on <=',$end_date);
+            $this->db->where('ppp.tanggal_invoice >=',$start_date);
+            $this->db->where('ppp.tanggal_invoice <=',$end_date);
         }
         if(!empty($supplier_id)){
             $this->db->where('ppp.supplier_id',$supplier_id);
