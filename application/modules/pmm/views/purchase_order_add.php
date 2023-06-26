6d266841180f015f03cb0018a559707f64142919
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-06-26 14:15:32 +0700

    update

--- application/modules/pmm/views/purchase_order_add.php
+++ application/modules/pmm/views/purchase_order_add.php
@@ -252,14 +252,6 @@
                                 </tfoot>
                             </div>
                             </table>
-                            <table class="table table-striped table-bordered"> 
-                                <tr>
-                                    <th class="text-left" colspan="6">Memo :</th>
-                                </tr>
-                                <tr>
-                                    <th class="text-left" colspan="6"><?php echo $data['memo'];?></th>
-                                </tr>
-                            </table>
                                 
                             <div class="text-right">
 
