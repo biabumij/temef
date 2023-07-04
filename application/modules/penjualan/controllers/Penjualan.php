f37fd5dfe895fac44ffec205e31367c8c538d50c
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-07-04 12:59:59 +0700

    update

--- application/modules/penjualan/controllers/Penjualan.php
+++ application/modules/penjualan/controllers/Penjualan.php
@@ -1716,59 +1716,6 @@ class Penjualan extends Secure_Controller
 
 	public function main_table()
 	{	
-		$data = $this->pmm_model->TableMainTagihanPenjualan($this->input->post('id'));
-		echo json_encode(array('data'=>$data));
-	}
-
-	public function get_tagihan_main()
-	{
-		$output['output'] = false;
-		$id = $this->input->post('id');
-		if(!empty($id)){
-            $data = $this->db->select('ppp.*')
-            ->from('pmm_penagihan_penjualan ppp')
-            ->where('ppp.id',$id)
-            ->get()->row_array();
-
-            $data['nama']= $this->crud_global->GetField('penerima',array('id'=>$data['client_id']),'nama');
-            $data['tanggal_invoice'] = date('d-m-Y',strtotime($data['tanggal_invoice']));
-			$output['output'] = $data;
-            
-		}
-		echo json_encode($output);
-	}
-
-	public function update_tagihan_main()
-	{
-		$output['output'] = false;
-
-		$penagihan_id = $this->input->post('penagihan_id');
-        $supplier_id = $this->input->post('nama');
-		$tanggal_invoice = date('Y-m-d',strtotime($this->input->post('tanggal_invoice')));
-        $nomor_invoice = $this->input->post('nomor_invoice');
-
-		$data = array(
-            'id' => $penagihan_id,
-		    'tanggal_invoice' => $tanggal_invoice,
-		);
-
-		if(!empty($id)){
-			if($this->db->update('pmm_penagihan_penjualan',$data,array('id'=>$penagihan_id))){
-				$output['output'] = true;
-			}
-		}else{
-            $data['updated_by'] = $this->session->userdata('admin_id');
-            $data['updated_on'] = date('Y-m-d H:i:s');
-			if($this->db->update('pmm_penagihan_penjualan',$data,array('id'=>$penagihan_id))){
-				$output['output'] = true;
-			}
-		}
-		
-		echo json_encode($output);	
-	}
-
-	public function main_table()
-	{	
 		$data = $this->pmm_model->TableMainKomposisi($this->input->post('id'));
 		echo json_encode(array('data'=>$data));
 	}
