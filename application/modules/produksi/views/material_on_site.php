e818d2fe4dc38be41ada50ac6c2cde541f6a427d
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-07-13 14:19:32 +0700

    update

--- application/modules/produksi/views/material_on_site.php
+++ application/modules/produksi/views/material_on_site.php
@@ -2,10 +2,10 @@
     <div class="col-sm-4">
         <input type="text" id="filter_date" name="filter_date" class="form-control dtpickerange" autocomplete="off" placeholder="Filter By Date">
     </div>
-    <div class="col-sm-1">
+    <div class="col-sm-2">
         <button type="submit" class="btn btn-info"><i class="fa fa-print"></i> Cetak PDF</button>
     </div>
-    <div class="col-sm-2">
+    <div class="col-sm-3">
         <a href="javascript:void(0);" onclick="OpenForm()" class="btn btn-primary"><i class="fa fa-plus"></i> Tambah Stock Opname</a>
     </div>
 </form>
