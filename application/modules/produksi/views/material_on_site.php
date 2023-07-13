a00fb04a5840675781e585da900607f7afbf27e5
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-07-13 14:14:02 +0700

    update

--- application/modules/produksi/views/material_on_site.php
+++ application/modules/produksi/views/material_on_site.php
@@ -1,11 +1,11 @@
 <form action="<?php echo site_url('produksi/cetak_stock_opname'); ?>" target="_blank">
-    <div class="col-sm-4">
+    <div class="col-sm-4" align="left">
         <input type="text" id="filter_date" name="filter_date" class="form-control dtpickerange" autocomplete="off" placeholder="Filter By Date">
     </div>
-    <div class="col-sm-3">
+    <div class="col-sm-3" align="left">
         <a href="javascript:void(0);" onclick="OpenForm()" class="btn btn-primary"><i class="fa fa-plus"></i> Tambah Stock Opname</a>
     </div>
-    <div class="col-sm-3">
+    <div class="col-sm-3" align="left">
         <button type="submit" class="btn btn-info"><i class="fa fa-print"></i> Cetak PDF</button>
     </div>
 </form>
