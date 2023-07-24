acc3c0b9ea8fa39a4f321640a2b4737cd414bcf0
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-07-24 11:32:09 +0700

    update

--- application/modules/pmm/views/finance/costs.php
+++ application/modules/pmm/views/finance/costs.php
@@ -165,7 +165,7 @@
             ],
             "columnDefs": [
                 {
-                    "targets": [0, 1, 2, 5],
+                    "targets": [0, 1, 5],
                     "className": 'text-center',
                 },
                 {
