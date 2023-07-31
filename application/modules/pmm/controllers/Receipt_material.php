4d431cdb5e7549d0f23b495053bbb3059ed510f4
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-07-31 19:32:18 +0700

    update

--- application/modules/pmm/controllers/Receipt_material.php
+++ application/modules/pmm/controllers/Receipt_material.php
@@ -470,12 +470,12 @@ class Receipt_material extends CI_Controller {
 
 		$receipt_material = $this->db->select('SUM(volume) as volume')->get_where('pmm_receipt_material',array('purchase_order_id'=>$purchase_order_id,'material_id'=>$material_id))->row_array();
 
-		if($receipt_material['volume'] + $volume > $get_po['volume']){
+		/*if($receipt_material['volume'] + $volume > $get_po['volume']){
 			$output['output'] = false;
 			$output['err'] = 'Mohon maaf Volume Penerimaan sudah melebihi TOTAL PO, Silahkan buat PO baru.';
 			echo json_encode($output);
 			exit();
-		}
+		}*/
 
 		$this->db->trans_start(); # Starting Transaction
 		$this->db->trans_strict(FALSE); # See Note 01. If you wish can remove as well 
