f5532fae89b551e7a7c01cb2fd7b89c43b0300d6
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-08-02 12:08:04 +0700

    update

--- application/modules/laporan_produksi/views/cetak_laporan_evaluasi_alat.php
+++ application/modules/laporan_produksi/views/cetak_laporan_evaluasi_alat.php
@@ -462,6 +462,22 @@
 				$pembelian_wl_sc_measure = $x['measure'];
 			}
 
+			$insentif_wl_sc = $this->db->select('pb.memo as memo, sum(pdb.debit) as total')
+			->from('pmm_jurnal_umum pb ')
+			->join('pmm_detail_jurnal pdb','pb.id = pdb.jurnal_id','left')
+			->where("pdb.akun = 221")
+			->where("pb.status = 'PAID'")
+			->where("pb.status = 'SC'")
+			->where("(pb.tanggal_transaksi between '$date1' and '$date2')")
+			->group_by('pdb.id')
+			->get()->result_array();
+
+			$total_insentif_wl_sc = 0;
+
+			foreach ($insentif_wl_sc as $y){
+				$total_insentif_wl_sc += $y['total'];
+			}
+
 			$total_vol_batching_plant = $total_volume;
 			$total_vol_truck_mixer = $total_volume;
 			$total_vol_wheel_loader = $total_volume;
@@ -483,7 +499,7 @@
 			$total_pemakaian_dmp_10m3 = $total_nilai_dmp_10m3;
 			$total_pemakaian_sc = $total_nilai_sc;
 			$total_pemakaian_gns = $total_nilai_gns;
-			$total_pemakaian_wl_sc = $total_nilai_wl_sc;
+			$total_pemakaian_wl_sc = $total_nilai_wl_sc + $total_insentif_wl_sc;
 			?>
 
 			<!-- RAP Alat -->
@@ -778,7 +794,7 @@
 	        </tr>
 			<tr class="table-baris1">
 				<th align="center">4.</th>			
-				<th align="left">Wheel Loader</th>
+				<th align="left">Wheel Loader + Insentif</th>
 				<th align="center"><?php
 				$null = '-';
 				if (!empty($pembelian_wl_sc_measure)) {
