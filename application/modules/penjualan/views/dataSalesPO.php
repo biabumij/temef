0ced89f93fe800d2b934453afae500ee52b457ab
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-08-08 11:36:07 +0700

    update

--- application/modules/penjualan/views/dataSalesPO.php
+++ application/modules/penjualan/views/dataSalesPO.php
@@ -68,7 +68,7 @@
                                 </tr>
                                 <tr>
                                     <th>Tanggal Kontrak</th>
-                                    <th><?= $sales_po["contract_number"]; ?></th>
+                                    <th><?= date('d/m/Y',strtotime($sales_po['contract_number']));?></th>
                                 </tr>
                                 <tr>
                                     <th>Memo</th>
