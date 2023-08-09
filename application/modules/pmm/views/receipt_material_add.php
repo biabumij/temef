48a7d31bf7f01d5d2ea7e86dde86a780d1592d97
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-08-09 10:56:10 +0700

    update

--- application/modules/pmm/views/receipt_material_add.php
+++ application/modules/pmm/views/receipt_material_add.php
@@ -391,8 +391,10 @@
             },
             //minDate: new Date()+0,
 			//maxDate: new Date()+1,
-            minDate: moment().add(-10, 'd').toDate(),
-			maxDate: moment().add(+0, 'd').toDate(),
+            //minDate: moment().add(-10, 'd').toDate(),
+			//maxDate: moment().add(+0, 'd').toDate(),
+            minDate: moment().startOf('month').toDate(),
+			maxDate: moment().endOf('month').toDate(),
         });
 
 
