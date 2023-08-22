0b78c4db9389befb3892da13e4647b99d4c5f73c
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-08-22 13:12:37 +0700

    update

--- application/modules/pmm/views/biaya/tambah_biaya.php
+++ application/modules/pmm/views/biaya/tambah_biaya.php
@@ -163,7 +163,7 @@
                                     </div>
                                     <div class="row">
                                         <div class="col-sm-12 text-right">
-                                            <a href="<?= site_url('admin/biaya_umum_&_adm.');?>" class="btn btn-danger" style="margin-bottom:0; width:15%; font-weight:bold;"><i class="fa fa-close"></i> Batal</a>
+                                            <a href="<?= site_url('admin/biaya_bua');?>" class="btn btn-danger" style="margin-bottom:0; width:15%; font-weight:bold;"><i class="fa fa-close"></i> Batal</a>
                                             <button type="submit" class="btn btn-success" style="width:15%; font-weight:bold;"><i class="fa fa-send"></i>  Kirim</button>
                                         </div>
                                     </div>
