106ea7fcfe905b3bce6a9dd1396326ee86d2ddd6
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-09-04 14:28:44 +0700

    update

--- application/modules/penjualan/views/index.php
+++ application/modules/penjualan/views/index.php
@@ -172,8 +172,8 @@
                                                 <br />
                                                 <div class="col-sm-6">
                                                     <div class="text-left">
-                                                        <button type="submit" class="btn btn-default" style="width:200px; font-weight:bold;"><i class="fa fa-print"></i> Print</button>
-                                                        <button type="button" id="btn_production" class="btn btn-success" style="width:200px; font-weight:bold;">Penagihan Penjualan</button>
+                                                        <button type="submit" class="btn btn-default" style="width:100px; font-weight:bold; border-radius:10px;"><i class="fa fa-print"></i> Print</button>
+                                                        <button type="button" id="btn_production" class="btn btn-success" style="width:200px; font-weight:bold; border-radius:10px;">Penagihan Penjualan</button>
                                                     </div>
                                                 </div>
                                                 <br />
@@ -259,7 +259,7 @@
                                             </div>
                                             <div class="col-sm-6">
                                                 <div class="text-left">
-                                                    <button type="submit" class="btn btn-default" style="width:200px; font-weight:bold;"><i class="fa fa-print"></i> Print</button>
+                                                    <button type="submit" class="btn btn-default" style="width:100px; font-weight:bold; border-radius:10px;"><i class="fa fa-print"></i> Print</button>
                                                 </div>
                                             </div>
                                         </form>
