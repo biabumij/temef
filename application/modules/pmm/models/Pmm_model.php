4497a94a21ab4fee9ef0ff3d5bc0cf8d0f01b65b
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-09-06 14:13:47 +0700

    update

--- application/modules/pmm/models/Pmm_model.php
+++ application/modules/pmm/models/Pmm_model.php
@@ -3734,7 +3734,7 @@ class Pmm_model extends CI_Model {
                 $row['total_kredit']= number_format($row['total_kredit'],0,',','.');
                 $row['memo']= $row['memo'];
                 $row['lampiran'] = "<a href=" . base_url('uploads/jurnal_umum/' . $row["lampiran"]) . ">" . $row["lampiran"] . "</a>";  
-                $row['actions'] = '<a href="javascript:void(0);" onclick="OpenFormMain('.$row['id'].')" class="btn btn-success">Update Jutrnal Umum </a>';
+                $row['actions'] = '<a href="javascript:void(0);" onclick="OpenFormMain('.$row['id'].')" class="btn btn-success" style="font-weight:bold; border-radius:10px;">Update Jutrnal Umum </a>';
                 
                 $data[] = $row;
             }
@@ -3761,7 +3761,7 @@ class Pmm_model extends CI_Model {
                 $row['deskripsi']= $row['deskripsi'];
 				$row['debit']= number_format($row['debit'],0,',','.');
                 $row['kredit']= number_format($row['kredit'],0,',','.');
-                $row['actions'] = '<a href="javascript:void(0);" onclick="DeleteData('.$row['id'].')" class="btn btn-danger"><i class="fa fa-close"></i> </a> <a href="javascript:void(0);" onclick="OpenForm('.$row['id'].')" class="btn btn-primary"><i class="fa fa-edit"></i> </a>';
+                $row['actions'] = '<a href="javascript:void(0);" onclick="DeleteData('.$row['id'].')" class="btn btn-danger" style="font-weight:bold; border-radius:10px;"><i class="fa fa-close"></i> </a> <a href="javascript:void(0);" onclick="OpenForm('.$row['id'].')" class="btn btn-primary" style="font-weight:bold; border-radius:10px;"><i class="fa fa-edit"></i> </a>';
                 
                 $data[] = $row;
             }
