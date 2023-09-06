f0d366603516c3190f12ce52e43749647c562ae3
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-09-06 14:10:11 +0700

    update

--- application/modules/pmm/views/jurnal_umum/detailJurnal.php
+++ application/modules/pmm/views/jurnal_umum/detailJurnal.php
@@ -137,8 +137,9 @@
                                         </table>
                                     </div>
                                 </div>
+                                <br /><br />
                                 <div class="row">
-                                        <div class="col-sm-12 text-right">
+                                        <div class="col-sm-12 text-center">
                                             <a href="<?= base_url('admin/jurnal_umum') ?>" class="btn btn-info" style="width:10%; font-weight:bold; border-radius:10px;"><i class="fa fa-arrow-left"></i> Kembali</a>
                                             <?php
                                             if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 10 || $this->session->userdata('admin_group_id') == 15){
