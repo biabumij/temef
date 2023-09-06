1193b12856bfceed710a45f4a3eafadefd66b38b
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-09-06 14:27:58 +0700

    update

--- application/modules/pmm/views/biaya/tambah_biaya.php
+++ application/modules/pmm/views/biaya/tambah_biaya.php
@@ -161,8 +161,9 @@
                                             <input type="hidden" name="total_product" id="total-product" value="1">
                                         </div>
                                     </div>
+                                    <br /><br />
                                     <div class="row">
-                                        <div class="col-sm-12 text-right">
+                                        <div class="col-sm-12 text-center">
                                             <a href="<?= site_url('admin/biaya_bua');?>" class="btn btn-danger" style="margin-bottom:0; width:10%; font-weight:bold; border-radius:10px;"><i class="fa fa-close"></i> Batal</a>
                                             <button type="submit" class="btn btn-success" style="width:10%; font-weight:bold; border-radius:10px;"><i class="fa fa-send"></i>  Kirim</button>
                                         </div>
