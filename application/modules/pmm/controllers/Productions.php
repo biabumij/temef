f91e14cc6df1a0cded0f56580b26aecb912f52b2
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-10-24 10:20:54 +0700

    update

--- application/modules/pmm/controllers/Productions.php
+++ application/modules/pmm/controllers/Productions.php
@@ -1186,7 +1186,7 @@ class Productions extends Secure_Controller {
         }
 		
 		$this->db->join('pmm_penagihan_penjualan ppp', 'pmp.penagihan_id = ppp.id','left');
-		$this->db->join('pmm_sales_po ppo', 'ppp.penagihan_id = ppo.id','left');
+		$this->db->join('pmm_sales_po ppo', 'ppp.sales_po_id = ppo.id','left');
 		$this->db->where("ppo.status in ('OPEN','CLOSED')");
 		$this->db->group_by('pmp.client_id');
 		$this->db->order_by('pmp.nama_pelanggan','asc');
