04f36786e6d7e9deeaa3b844b47ab0390e9c4727
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-11-09 16:06:42 +0700

    update

--- application/modules/pmm/views/biaya/detail_biaya.php
+++ application/modules/pmm/views/biaya/detail_biaya.php
@@ -15,14 +15,12 @@
 
 <body>
     <div class="wrap">
-        
         <?php echo $this->Templates->PageHeader();?>
-
         <div class="page-body">
             <?php echo $this->Templates->LeftBar();?>
-            <div class="content" style="padding:0;">
+            <div class="content">
                 <div class="content-header">
-                    <div class="leftside-content-header">
+                    <div class="leftside-content-header">>
                         <ul class="breadcrumbs">
                             <li><i class="fa fa-sitemap" aria-hidden="true"></i><a href="<?php echo site_url('admin');?>">Dashboard</a></li>
                             <li>
