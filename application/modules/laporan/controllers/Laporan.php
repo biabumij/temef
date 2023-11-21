8b84d484e7542d41c595a02fe62d68c389871a3f
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-11-21 15:28:20 +0700

    update

--- application/modules/laporan/controllers/Laporan.php
+++ application/modules/laporan/controllers/Laporan.php
@@ -89,9 +89,13 @@ class Laporan extends Secure_Controller {
 			$filter_date = '-';
 		}else {
 			$arr_filter_date = explode(' - ', $arr_date);
+			$start_date = date('Y-m-d',strtotime($arr_filter_date[0]));
+			$end_date = date('Y-m-d',strtotime($arr_filter_date[1]));
 			$filter_date = date('d F Y',strtotime($arr_filter_date[0])).' - '.date('d F Y',strtotime($arr_filter_date[1]));
 		}
 		$data['filter_date'] = $filter_date;
+		$data['start_date'] = $start_date;
+		$data['end_date'] = $end_date;
         $html = $this->load->view('laporan_keuangan/cetak_bahan_akumulasi',$data,TRUE);
 
         $pdf->SetTitle('BBJ - Bahan');
