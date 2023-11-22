156d77456c29c953ea67c915b498d1c8ceb03bb4
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-11-22 13:41:15 +0700

    update

--- application/modules/pembelian/views/penawaran_pembelian_detail.php
+++ application/modules/pembelian/views/penawaran_pembelian_detail.php
@@ -203,9 +203,11 @@
                                     <?php
                                         if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 10 || $this->session->userdata('admin_group_id') == 11|| $this->session->userdata('admin_group_id') == 16){
                                         ?>
+                                            <br /><br />
                                             <form class="form-check" action="<?= base_url("pembelian/closed_penawaran_pembelian/".$row["id"]) ?>">
-                                            <button type="submit" class="btn btn-danger" style="width:200px; font-weight:bold; border-radius:10px;"><i class="fa fa-briefcase"></i> Closed</button>      
+                                                <button type="submit" class="btn btn-danger" style="width:200px; font-weight:bold; border-radius:10px;"><i class="fa fa-briefcase"></i> Closed</button>      
                                             </form>
+                                            <br />
                                             <form class="form-check" action="<?= base_url("pembelian/reject_penawaran_pembelian/".$row["id"]) ?>">
                                                 <button type="submit" class="btn btn-warning" style="width:200px; font-weight:bold; border-radius:10px;"><i class="fa fa-close"></i> Reject</button>        
                                             </form>		
